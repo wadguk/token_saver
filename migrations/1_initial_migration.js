af81b0fe7a788fe5fea057d398545877f0373aa5
wadguk <57893892+wadguk@users.noreply.github.com>
2020-01-27 12:59:59 +0300

    last version added

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,25 @@
+const TokenSaverTest = artifacts.require("./contracts/TokenSaverTest.sol");
+const ERC20 = artifacts.require("./ERC20.sol");
+const timeHelper = require('../test/utils/utils.js');
+
+(async function () {
+    await timeHelper.revertToSnapShot('0x1');                           //revert time
+})()
+
+const getAccount = async (i) => {
+    try {
+        const myAccounts = await web3.eth.getAccounts();
+        return myAccounts[i];
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = async function (deployer) {
+    deployer.deploy(TokenSaverTest, getAccount(0), getAccount(1),
+        Math.floor(Date.now() / 1000) + 150000);                         // Data shift
+    console.log('TIME NOW:', now = new Date());
+    deployer.deploy(ERC20, 10000);
+};
+
+
